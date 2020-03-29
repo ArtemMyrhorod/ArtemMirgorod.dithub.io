66304434d71bf22fd9f57cb61559736a7561fb4a
ArtemMirgorod <62835148+ArtemMirgorod@users.noreply.github.com>
2020-03-29 14:33:50 +0200

    page layout

---
+++ start/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $('.slider__inner').slick({
+    	 dots: true,
+ autoplay: true,
+ autoplaySpeed: 2000,
+ arrows: false,
+  focusOnSelect: true,
+  pauseOnFocus: false,
+  infinite: true,
+  dotsClass: 'dots-style'
+    });
+   
+
+});
