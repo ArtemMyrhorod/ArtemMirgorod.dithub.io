ae999f37ce8655e38f4d7b5c4f03cac42b330564
ArtemMirgorod <62835148+ArtemMirgorod@users.noreply.github.com>
2020-09-12 21:05:48 +0200

    portfolio site

---
+++ Portfolio One Screen/js/styleSwitcher.js
@@ -0,0 +1,19 @@
+const links=document.querySelectorAll(".alternate-style"),
+totalLinks=links.length;
+
+
+
+function setActiveStyle(color) {
+	for(let i=0; i<totalLinks; i++) {
+		if(color === links[i].getAttribute("title")) {
+			links[i].removeAttribute("disabled");
+		}
+		else{
+			links[i].setAttribute("disabled", "true");
+		}
+	}
+}
+
+document.querySelector(".toggle-style-Switcher").addEventListener("click",() => {
+	document.querySelector(".style-switcher").classList.toggle("open");
+})
